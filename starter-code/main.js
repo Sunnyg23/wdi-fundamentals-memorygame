ea067a7a6aed4e80c14237f2ce1e1729192e127c
Sunbir Ghag <sunbirghag@sunbirs-mbp.home>
2017-03-08 20:44:09 +0000

    created page emessage

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,3 +5,12 @@ var  cardOne = "queen";
 var  cardTwo = "queen";
 var  cardThree = "king";
 var  cardFour = "king";
+
+
+if (cardOne === cardTwo){
+      alert("Match Found!")
+}else if (cardThree === cardFour){
+	  alert("Match Found")
+}else{
+      alert("Sorry,try again!.");
+}
