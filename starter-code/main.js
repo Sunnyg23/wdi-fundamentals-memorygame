4df70ddcd86095e6f86520fb10eed9b1944511fc
Sunbir Ghag <sunbirghag@sunbirs-mbp.home>
2017-03-09 18:58:03 +0000

    unit 10

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,16 +1,38 @@
 
-console.log("JS file is connected to HTML! Woo!")
+//console.log("JS file is connected to HTML! Woo!")
 
-var  cardOne = "queen";
-var  cardTwo = "queen";
-var  cardThree = "king";
-var  cardFour = "king";
+//var  cardOne = "queen";
+//var  cardTwo = "queen";
+//var  cardThree = "king";
+//var  cardFour = "king";
 
 
-if (cardOne === cardTwo){
-      alert("Match Found!")
-}else if (cardThree === cardFour){
-	  alert("Match Found")
-}else{
-      alert("Sorry,try again!.");
+//if (cardOne === cardTwo){
+     // alert("Match Found!")
+//}else if (cardThree === cardFour){
+	 // alert("Match Found")
+//}else{
+      //alert("Sorry,try again!.");
+//}
+// find the board and set it to a variable
+// so that we can add cards to it
+
+var board = document.getElementById('game-board');
+
+
+function createBoard() {
+
+  for (var i=0; i<cards.length; i++) {
+
+    
+    var cardElement = document.createElement('div');
+
+   
+    cardElement.className = 'card';
+
+  
+    board.appendChild(cardElement);
+  }
+
 }
+
